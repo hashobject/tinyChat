5ef8a7f4b462a609533d4f9d0d9ae31eea1577ee
Anton Podviaznikov <podviaznikov@gmail.com>
2014-02-08 12:46:31 +0200

    initial setup

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    stylus: {
+      compile: {
+        options: {
+          paths: ['styl'],
+          urlfunc: 'url',
+          'include css': true,
+          compress: false
+        },
+        files: {
+          'dist/css/app.css': 'styl/app.styl'
+        }
+      }
+    },
+
+    jade: {
+      compile: {
+        options: {
+          pretty: true,
+          data: {
+            debug: true
+          }
+        },
+        files: {
+          "dist/index.html": ["views/index.jade"]
+        }
+      }
+    },
+
+
+
+    watch: {
+      src: {
+        files: ['styl/*.styl', 'views/*.jade'],
+        tasks: ['build']
+      }
+    }
+
+
+
+  });
+
+
+
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  grunt.registerTask('build', ['jade:compile', 'stylus:compile']);
+
+};
